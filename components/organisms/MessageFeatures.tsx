4fad2e2cea552279aee041fd2ddbc995a060a366
regginssa <randyarabis428@gmail.com>
2026-06-30 02:23:18 +0900

    Add MessageFeatures

---
+++ components/organisms/MessageFeatures.tsx
@@ -0,0 +1,11 @@
+const features = [
+  { label: "Active timeline", value: "active_timeline", icon: "" },
+];
+
+export default function MessageFeatures() {
+  return (
+    <ul className="space-y-4">
+      <li className=""></li>
+    </ul>
+  );
+}
