ba53c358a36833590c336eb114279ccf1f7d1630
regginssa <randyarabis428@gmail.com>
2026-07-01 17:04:13 +0900

    Add Category to JobFilter

--- components/organisms/JobFilter.tsx
+++ components/organisms/JobFilter.tsx
@@ -1,6 +1,13 @@
+import CategoriesAPI from "@/lib/api/categories";
 import CheckBoxGroup from "../molecules/CheckBoxGroup";
 import { Icon } from "@iconify/react";
 import { AnimatePresence, motion } from "motion/react";
+import { useQuery } from "@tanstack/react-query";
+import { CheckboxGroupDropdown } from "../atoms";
+import { useState } from "react";
+import SearchableGroupDropdown, {
+  SearchableGroupOption,
+} from "../atoms/SearchableGroupDropdown";
 
 const FILTER_SECTIONS = [
   "category",
@@ -75,9 +82,55 @@ const locationOptions = [
 ];
 
 export default function JobFilter() {
+  const [filterData, setFilterData] = useState<any>({});
+  const [expandedSections, setExpandedSections] = useState<
+    Record<FilterSectionKey, boolean>
+  >(
+    Object.fromEntries(FILTER_SECTIONS.map((key) => [key, true])) as Record<
+      FilterSectionKey,
+      boolean
+    >
+  );
+
+  const toggleSection = (key: FilterSectionKey) => {
+    setExpandedSections((prev) => ({ ...prev, [key]: !prev[key] }));
+  };
+  const { data: categories } = useQuery({
+    queryKey: ["categories"],
+    queryFn: CategoriesAPI.getAll,
+  });
+
+  const makeCategoryOptions = (): SearchableGroupOption[] => {
+    const options: SearchableGroupOption[] = [];
+    categories?.forEach((category) => {
+      options.push({
+        title: category.name,
+        items: category.children.map((child) => ({
+          label: child.name,
+          value: child.slug,
+        })),
+      });
+    });
+    return options;
+  };
+
   return (
     <div className="space-y-4">
       <CheckBoxGroup options={locationOptions} value={[]} onChange={() => {}} />
+
+      <FilterSection
+        key="category"
+        title="Category"
+        expanded={expandedSections.category}
+        onToggle={() => toggleSection("category")}
+      >
+        <SearchableGroupDropdown
+          name="category"
+          options={makeCategoryOptions()}
+          values={filterData.category ?? []}
+          onChange={() => {}}
+        />
+      </FilterSection>
     </div>
   );
 }
